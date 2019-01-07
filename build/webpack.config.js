dd3c59023f59bc00d0f5b9e2275066421b8ea7ac
Jason Sheng <shengminming2011@gmail.com>
2019-01-07 18:05:27 -0500

    Initial Commit

---
+++ build/webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let path = require('path');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, '../src/app/index.js')
+    },
+    output: {
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {minimize: false}
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, '../src/app/index.html'),
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    resolve: {
+        alias: {
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 9090
+    }
+};
